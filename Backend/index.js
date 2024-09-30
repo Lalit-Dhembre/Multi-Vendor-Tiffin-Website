0e034ba4f95bb7068b9dfe01ec631a65af42d556
Lalit <lalitdhembre6@gmail.com>
2024-09-30 19:49:10 +0530

    MongoDb Connection Succesful

--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,12 @@
 const express = require("express")
 const cors = require("cors")
 const dotenv = require("dotenv")
+const mongoose = require("mongoose");
 
+
+mongoose.connect(process.env.MONGO_URL.toString)
+.then(() => console.log("DATABASE CONNECTED!"))
+.catch((err) => console.log(err))
 const app = express()
 const PORT = 3000
 
